11c577f6406137bef34b1417048dfc9470b0c81b
TiesMorshuis <Tiesmorshuis@gmail.com>
2024-01-29 22:44:44 +0100

    EPD-1-2 Voorbereiding

---
+++ EPD-1-2/EPD-1-2-V/EPD-1-2-V.ino
@@ -0,0 +1,26 @@
+const int pin_6 = 6;
+const int pin_7 = 7;
+const int pin_8 = 8;
+const int pin_9 = 9;
+const int pin_10 = 10;
+const int pin_11 = 11;
+const int pin_12 = 12;
+const int pin_13 = 13;
+
+void setup() {
+  pinMode(pin_6, OUTPUT);
+  pinMode(pin_7, OUTPUT);
+  pinMode(pin_8, OUTPUT);
+  pinMode(pin_9, OUTPUT);
+  pinMode(pin_10, OUTPUT);
+  pinMode(pin_11, OUTPUT);
+  pinMode(pin_12, OUTPUT);
+  pinMode(pin_13, OUTPUT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  knipperen();
+}
---
+++ EPD-1-2/EPD-1-2-V/Knipperen_void.ino
@@ -0,0 +1,26 @@
+void knipperen(){
+  digitalWrite(pin_13, HIGH);
+  delay(200);
+  digitalWrite(pin_13, LOW);
+  digitalWrite(pin_12, HIGH);
+  delay(200);
+  digitalWrite(pin_12, LOW);
+  digitalWrite(pin_11, HIGH);
+  delay(200);
+  digitalWrite(pin_11, LOW);
+  digitalWrite(pin_10, HIGH);
+  delay(200);
+  digitalWrite(pin_10, LOW);
+  digitalWrite(pin_9, HIGH);
+  delay(200);
+  digitalWrite(pin_9, LOW);
+  digitalWrite(pin_8, HIGH);
+  delay(200);
+  digitalWrite(pin_8, LOW);
+  digitalWrite(pin_7, HIGH);
+  delay(200);
+  digitalWrite(pin_7, LOW);
+  digitalWrite(pin_6, HIGH);
+  delay(200);
+  digitalWrite(pin_6, LOW);
+}
